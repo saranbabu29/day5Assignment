2537eecc5c8e21d630d8d8befab68ac06090c66c
saranbabu29 <sharanraj982@gmail.com>
2022-07-31 19:23:16 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace day5
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            int headcount = 0;
+            int tailcount = 0;
+            int count = 6;
+
+            int temp = count;
+            Random r = new Random();
+            while (count != 0)
+            {
+                double random = r.NextDouble();
+                if (random <= 0.5)
+                {
+                    headcount = headcount + 1;
+                }
+                else
+                {
+                    tailcount++;
+                }
+
+                count = count - 1;
+
+            }
+            Console.WriteLine(headcount * 100 / temp);
+            Console.WriteLine(tailcount * 100 / temp);
+
+        }
+    }
+}
